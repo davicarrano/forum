672f43bea1103629e5e157206e2386119e1d9cb2
davi <davi@ufsj.edu.br>
2020-01-13 13:43:11 -0300

    primeiro commit

---
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -0,0 +1,18 @@
+package br.com.alura.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@SpringBootApplication
+@EnableSpringDataWebSupport
+@EnableCaching
+public class ForumApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
